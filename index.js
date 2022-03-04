2653ab96b74415dae02aacc3cefdfb5f0c00c322
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2022-03-05 00:07:23 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// 3.
+
+// let M = 500;
+// M = M / 1000;
+
+// console.log(`massa: ${M} tonna`);
+
+//6.
+
+// let a=45;
+// console.log(`Onliklar xonasidagi raqam: ${parseInt(a/10)}`);
+// console.log(`Onliklar xonasidagi raqam: ${(a%10)}`);
+
+// 12.
+
+// let a = 123;
+// console.log(a);
+
+// // 321
+// let b = Math.floor(a / 100); //1
+// let c = Math.floor(a / 10) - 10 * b; //2
+// let d = a % 10; //3
+
+// console.log(d*100+c*10+b);
+
+
+// 17.
+
+// 8763 -> 7
+
+// let a=8763;
+
+// let yuzlarXonasiSon=Math.floor(a/100)-10*Math.floor(a/1000);
+
+// console.log(yuzlarXonasiSon);
+
+
+
+
